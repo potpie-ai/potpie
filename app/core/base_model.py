3f01ad9a80c66274ff3fec9491e31a64331888d2
Vineet <vn.sh@outlook.com>
2024-09-12 08:16:36 +0530

    model fixes

--- app/core/base_model.py
+++ app/core/base_model.py
@@ -1,3 +1,15 @@
-from sqlalchemy.ext.declarative import declarative_base
+import typing as t
 
-Base = declarative_base()
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+
+class_registry: t.Dict = {}
+
+@as_declarative(class_registry=class_registry)
+class Base:
+    id: t.Any
+    __name__: str
+
+    # Generate __tablename__ automatically
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
---
+++ app/core/models.py
@@ -0,0 +1,9 @@
+from app.core.database import Base
+from app.modules.conversations.conversation.conversation_model import Conversation #noqa
+from app.modules.conversations.message.message_model import Message #noqa
+from app.modules.intelligence.prompts.prompt_model import AgentPromptMapping, Prompt #noqa
+from app.modules.projects.projects_model import Project #noqa
+from app.modules.search.search_models import SearchIndex #noqa
+from app.modules.tasks.task_model import Task #noqa
+from app.modules.users.user_model import User  #noqa
+from app.modules.users.user_preferences_model import UserPreferences #noqa
