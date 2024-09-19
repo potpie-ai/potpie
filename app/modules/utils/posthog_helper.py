29110f39d2793bcd77fc479768358259f7adf48c
GodReaper <brajutso@gmail.com>
2024-09-19 13:18:40 +0530

    posthog and sentry disabled in derv

--- app/modules/utils/posthog_helper.py
+++ app/modules/utils/posthog_helper.py
@@ -5,10 +5,12 @@ from posthog import Posthog
 
 class PostHogClient:
     def __init__(self):
+        self.is_development = os.getenv("ENV") == "development"
         self.api_key = os.getenv("POSTHOG_API_KEY")
         self.posthog_host = os.getenv("POSTHOG_HOST")
         self.posthog = Posthog(self.api_key, host=self.posthog_host)
 
+    
     def send_event(self, user_id: str, event_name: str, properties: dict):
         """
         Sends a custom event to PostHog.
@@ -16,7 +18,11 @@ class PostHogClient:
             user_id (str): The ID of the user performing the action.
             event_name (str): The name of the event to track.
             properties (dict): Additional properties related to the event.
+            
         """
+        if self.is_development:
+            return
+        
         try:
             self.posthog.capture(
                 user_id,  # User's unique identifier
