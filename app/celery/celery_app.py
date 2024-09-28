3116f06d80ce64f5399cc1cbea6be9fefccb6ff5
dhirenmathur <dhiru17398@gmail.com>
2024-09-28 19:01:56 +0530

    add import back

--- app/celery/celery_app.py
+++ app/celery/celery_app.py
@@ -65,3 +65,6 @@ configure_celery(queue_name)
 
 # Import tasks to ensure they are registered
 import app.celery.tasks.parsing_tasks  # noqa # Ensure the task module is imported
+
+# Import the lock decorator
+from celery.contrib.abortable import AbortableTask  # noqa
