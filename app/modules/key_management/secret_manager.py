4d87249d52046c1172ad6224b788f56686d5e031
God Reaper <brajutso@gmail.com>
2024-10-11 15:16:36 +0530

    Revert "secret manager made async"
    This reverts commit f4acc22b686bc3668419b6f86e18da32f2deaa8a.

--- app/modules/key_management/secret_manager.py
+++ app/modules/key_management/secret_manager.py
@@ -20,7 +20,7 @@ router = APIRouter()
 
 class SecretManager:
     @staticmethod
-    async def get_client_and_project():
+    def get_client_and_project():
         if os.getenv("isDevelopmentMode") == "disabled":
             client = secretmanager.SecretManagerServiceClient()
             project_id = os.environ.get("GCP_PROJECT")
@@ -30,13 +30,13 @@ class SecretManager:
         return client, project_id
 
     @router.post("/secrets")
-    async def create_secret(
+    def create_secret(
         request: CreateSecretRequest,
         user=Depends(AuthService.check_auth),
         db: Session = Depends(get_db),
     ):
         customer_id = user["user_id"]
-        client, project_id = await SecretManager.get_client_and_project()
+        client, project_id = SecretManager.get_client_and_project()
 
         # Update user preferences
         user_pref = (
@@ -51,7 +51,7 @@ class SecretManager:
         db.commit()
 
         api_key = request.api_key
-        secret_id = await SecretManager.get_secret_id(request.provider, customer_id)
+        secret_id = SecretManager.get_secret_id(request.provider, customer_id)
         parent = f"projects/{project_id}"
 
         secret = {"replication": {"automatic": {}}}
@@ -72,7 +72,7 @@ class SecretManager:
         return {"message": "Secret created successfully"}
 
     @staticmethod
-    async def get_secret_id(provider: Literal["openai", "anthropic"], customer_id: str):
+    def get_secret_id(provider: Literal["openai", "anthropic"], customer_id: str):
         if provider == "openai":
             secret_id = f"openai-api-key-{customer_id}"
         elif provider == "anthropic":
@@ -82,7 +82,7 @@ class SecretManager:
         return secret_id
 
     @router.get("/secrets/{provider}")
-    async def get_secret_for_provider(
+    def get_secret_for_provider(
         provider: Literal["openai", "anthropic"],
         user=Depends(AuthService.check_auth),
         db: Session = Depends(get_db),
@@ -99,12 +99,12 @@ class SecretManager:
                 status_code=404, detail="Secret not found for this provider"
             )
 
-        return await SecretManager.get_secret(provider, customer_id)
+        return SecretManager.get_secret(provider, customer_id)
 
     @staticmethod
-    async def get_secret(provider: Literal["openai", "anthropic"], customer_id: str):
-        client, project_id = await SecretManager.get_client_and_project()
-        secret_id = await SecretManager.get_secret_id(provider, customer_id)
+    def get_secret(provider: Literal["openai", "anthropic"], customer_id: str):
+        client, project_id = SecretManager.get_client_and_project()
+        secret_id = SecretManager.get_secret_id(provider, customer_id)
         name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
 
         try:
@@ -118,16 +118,15 @@ class SecretManager:
             )
 
     @router.put("/secrets/")
-    async def update_secret(
+    def update_secret(
         request: UpdateSecretRequest,
         user=Depends(AuthService.check_auth),
         db: Session = Depends(get_db),
     ):
-        
         customer_id = user["user_id"]
         api_key = request.api_key
-        secret_id = await SecretManager.get_secret_id(request.provider, customer_id)
-        client, project_id = await SecretManager.get_client_and_project()
+        secret_id = SecretManager.get_secret_id(request.provider, customer_id)
+        client, project_id = SecretManager.get_client_and_project()
         parent = f"projects/{project_id}/secrets/{secret_id}"
         version = {"payload": {"data": api_key.encode("UTF-8")}}
         client.add_secret_version(
@@ -149,14 +148,14 @@ class SecretManager:
         return {"message": "Secret updated successfully"}
 
     @router.delete("/secrets/{provider}")
-    async def delete_secret(
+    def delete_secret(
         provider: Literal["openai", "anthropic"],
         user=Depends(AuthService.check_auth),
         db: Session = Depends(get_db),
     ):
         customer_id = user["user_id"]
-        secret_id = await SecretManager.get_secret_id(provider, customer_id)
-        client, project_id = await SecretManager.get_client_and_project()
+        secret_id = SecretManager.get_secret_id(provider, customer_id)
+        client, project_id = SecretManager.get_client_and_project()
         name = f"projects/{project_id}/secrets/{secret_id}"
 
         try:
