25c922f6e7a9f15d87c9763da70530d33ded9c81
dhirenmathur <dhiru17398@gmail.com>
2025-02-25 17:56:45 +0530

    remove comment

--- app/modules/intelligence/provider/provider_service.py
+++ app/modules/intelligence/provider/provider_service.py
@@ -305,7 +305,7 @@ class ProviderService:
         """
         provider = self._get_provider_config(size)
         params = self._build_llm_params(provider, size)
-        routing_provider = params.pop("routing_provider", None)  # Remove routing_provider from params
+        routing_provider = params.pop("routing_provider", None)
         extra_params = {}
         if self.portkey_api_key and routing_provider != "ollama":
             # ollama + portkey is not supported currently
@@ -358,7 +358,7 @@ class ProviderService:
         """
         provider = self._get_provider_config(size)
         params = self._build_llm_params(provider, size)
-        routing_provider = params.pop("routing_provider", None)  # Remove routing_provider from params
+        routing_provider = params.pop("routing_provider", None)
 
         extra_params = {}
         if self.portkey_api_key and routing_provider != "ollama":
