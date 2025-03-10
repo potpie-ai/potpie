e67fc339e2c5919042b7657b7686106613f7d5d6
Nandan <nandan@momentum.sh>
2025-03-11 00:07:23 +0530

    fix: github service chardet

--- app/modules/code_provider/github/github_service.py
+++ app/modules/code_provider/github/github_service.py
@@ -7,6 +7,7 @@ from concurrent.futures import ThreadPoolExecutor
 from typing import Any, Dict, List, Optional, Tuple
 
 import aiohttp
+import chardet
 import git
 import requests
 from fastapi import HTTPException
