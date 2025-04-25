56a3c7ae7d57297ca4c31dd1362ab75bce34f360
Aniket Batabyal <31433194+anarchymonkey@users.noreply.github.com>
2025-04-25 16:06:34 +0700

    added graceful error handling in auth_router and auth_service (#380)

--- app/modules/auth/auth_service.py
+++ app/modules/auth/auth_service.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from fastapi import Depends, HTTPException, Request, Response, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from firebase_admin import auth
+from firebase_admin.exceptions import FirebaseError
 
 load_dotenv(override=True)
 
@@ -32,9 +33,20 @@ class AuthService:
             logging.exception(f"{log_prefix} {str(e)}")
             raise Exception(user_auth_response.json())
 
-    def signup(self, email, password, name):
-        user = auth.create_user(email=email, password=password, display_name=name)
-        return user
+    def signup(self, email: str, password: str, name: str) -> tuple:
+        try:
+            user = auth.create_user(
+                email=email, 
+                password=password, 
+                display_name=name
+            )
+            return { "user": user, "message": "New user created successfully" }, None
+        except FirebaseError as fe:
+            return None,  { "error": f"Firebase error: {fe.message}"}
+        except ValueError as _ve:
+            return None, { "error":  "Invalid input data provided." }       
+        except Exception as e:
+            return None, { "error": f"An unexpected error occurred: {str(e)}" }
 
     @classmethod
     @staticmethod
