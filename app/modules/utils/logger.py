1f5c19f2b915c1f6a0ff0d33d1225f87ca91470d
Nandan <nandan@momentum.sh>
2025-04-26 14:05:34 +0530

    feat: stop logger propogation to root logger

--- app/modules/utils/logger.py
+++ app/modules/utils/logger.py
@@ -5,6 +5,7 @@ def setup_logger(name):
     logger = logging.getLogger(name)
     if not logger.handlers:
         logger.setLevel(logging.INFO)
+        logger.propagate = False  # Prevent propogation to root logger
         handler = logging.StreamHandler()
         formatter = logging.Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
