c0aeb6a4c14e599db1641b7640936a115a9b4066
Nandan <41815448+nndn@users.noreply.github.com>
2025-09-15 17:25:49 +0530

    fix: register models (#439)
    Co-authored-by: nndn <guru.nandan.udupi@gmail.com>

--- app/core/models.py
+++ app/core/models.py
@@ -2,12 +2,14 @@ from app.modules.conversations.conversation.conversation_model import (  # noqa
     Conversation,
 )
 from app.modules.conversations.message.message_model import Message  # noqa
+from app.modules.media.media_model import MessageAttachment  # noqa
 from app.modules.intelligence.prompts.prompt_model import (  # noqa
     AgentPromptMapping,
     Prompt,
 )
 from app.modules.intelligence.agents.custom_agents.custom_agent_model import (  # noqa
     CustomAgent,
+    CustomAgentShare,
 )
 from app.modules.projects.projects_model import Project  # noqa
 from app.modules.search.search_models import SearchIndex  # noqa
